d6f5080094c12c81c4a84235f9d4ecd5e3acbda1
Annes S <47981031+annes99@users.noreply.github.com>
2019-04-02 14:16:46 +0700

    add array of quote objects

--- js/script.js
+++ js/script.js
@@ -3,54 +3,53 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
-
-
-/*** 
-  Create the array of quote objects and name it `quotes`.
-  Add at least five quote objects to the `quotes` array.
-  Give each quote object a `quote` and `source` property.
-  Add the `citation` property to at least one object in the array.
-  Add the `year` property to at least one object in the array.
-  Use console.log() to log your array of quotes to the console.
-***/
-
-
-
-
-/***
-  Create the `getRandomQuote` function to:
-   - Create a variable to store a random number 
-   - Cse the random number to `return` a random quote object from the `quotes` array.
-***/
-
-
-
-
-/***
-  Create the `printQuote` function to: 
-   - Call the `getRandomQuote` function and assign it to a variable.
-   - Create a variable for the HTML string and set it equal to an empty string.
-   - Use the HTML template in the instructions or the markup in the index.html file, AND 
-     the random quote vairable to build your HTML string.
-   - Add the quote and source section to the HTML string.
-   - Use an if statement to check for the citation property before adding it to the HTML string.
-   - Use an if statement to check for the year property before adding it to the HTML string.
-   - Don't forget to close that final `p` tag.
-   - Set the `innerHTML` of the `quote-box` div to the HTML string. 
-***/
-
-
-
-
-/***
-  When the "Show another quote" button is clicked, the event listener 
-  below will be triggered, and it will call, or "invoke", the `printQuote` 
-  function. So do not make any changes to the line of code below this 
-  comment.
-***/
-
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+// creating array of objects (quotes to randomly display on webpage)
+const quotes = [
+  {
+      quote: "You wanna make an omelet, you gotta break some eggs.",
+      source: "Tyler Durden",
+      citation: "Fight Club",
+      tags: "life, experience"
+  },
+  {
+      quote: "Fate, it seems, is not without a sense of irony.",
+      source: "Morpheus",
+      citation: "The Matrix",
+      tags: "life"
+  },
+  {
+      quote: "E = mc²",
+      source: "Albert Einstein",
+      year: 1905
+  },
+  {
+      quote: "All I need are some tasty waves, a cool buzz, and I'm fine.",
+      source: "Jeff Spicoli",
+      tags: "life, inspirational"
+  },
+  {
+      quote: "Perhaps home is not a place but simply an irrevocable condition.",
+      source: "James Baldwin",
+      year: 1956,
+      tags: "life, wisdom"
+  },
+  {
+      quote: "Our greatest glory consists not in never falling, but in rising every time we fall",
+      source: "Oliver Goldsmith",
+      tags: "life"
+  },
+  {
+      quote: "Economy is essential to all good fart.",
+      source: "Jerry Seinfeld",
+      citation: "Fresh Air",
+      year: 2007,
+      tags: "humor, wisdom"
+  },
+  {
+      quote: "It is a poor sport that is not worth the candle.",
+      source: "George Herbert",
+      citation: "Jacula Prudentum",
+      year: 1651,
+      tags: "sports"
+  }
+];
